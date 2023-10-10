eacbad4590fe755f0d5d5024253cc23e3910bc70
Pia <pia.koester@hey.com>
2023-10-10 21:24:35 +0200

    feat: add possibility to save list names and the selected List id to local storage

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const listsContainer = document.querySelector("[data-lists]");
 const newListForm = document.querySelector("[data-new-list-form]");
 const newListInput = document.querySelector("[data-new-list-input]");
 
+listsContainer.addEventListener("click", (e) => { // checking if user has clicked on a li item
+    if (e.target.tagName.toLowerCase() === "li") {
+        selectedListId = e.target.dataset.listId; // if li is clicked the dataset will get the id we created in the render function
+        saveAndRender()
+    }
+})
 
 const LOCAL_STORAGE_LIST_KEY = "task.lists"
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId" // creates key value pair for local storage to identify the active list
 
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [] // checks if the key already exists in the local storage and if yes then it gets them if not it uses an empty array
 console.log(lists);
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY) // checks local storage for the active list id
 
 
 newListForm.addEventListener("submit", (e) => {
@@ -24,12 +32,13 @@ function createList(name) { // this function creates a new list name and gives i
 }
 
 function saveAndRender() { // saves all the lists to local storage and calls the other functions
-    save()
+    save() 
     render()
 }
 
-function save() { // this function saves the list array as a string into local storage
-    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+function save() { // this function saves the list array as a string into local storage 
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists)); //Local... is the key and json stringify is the value
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId); // key is local.. and value is the id we created using the date in the render function
 }
 
 function render() { // this function creates the list and makes it a child of the ul
@@ -39,6 +48,9 @@ function render() { // this function creates the list and makes it a child of th
         listElement.dataset.listId = list.id;
         listElement.classList.add("list-name");
         listElement.innerText = list.name;
+        if (list.id === selectedListId) {
+            listElement.classList.add("active-list"); // gives the selected list the class of active so that the styling is different
+        }
         listsContainer.appendChild(listElement);
     }
 
